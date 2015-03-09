2f2f84dec6c9c7c556bbc4de1a4f9de723369796
wondachue <megddwyer@gmail.com>
2015-03-08 23:33:21 -0400

    Created infinite scrolling of every anime show in existence. Also created formatting of box with 'rolls', and links to the respective anime's wiki page

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+var numAnime = 0;
+
 var show_list = [
       "Hamtaro",
       "Pokemon",
@@ -10,13 +12,30 @@ $(function() {
   });
 $(document).ready(function() { 
   console.log("ready!");
-  loadXMLDoc();
+  getAllAnimeYears();
 });
-function loadXMLDoc()
+function createTable(){
+  var table = document.getElementById("show_grid");
+  for(var cell = 0; cell < show_list.length; cell+=2){
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    row.className = "row";
+    cell1.innerHTML = "<div class='box'><div class='innerbox'><div class='col'><div class='roll'><a href='http://en.wikipedia.org/wiki/" + show_list[cell] + "'>" + show_list[cell] + "</a></div></div></div></div>";
+    cell2.innerHTML = "<div class='box'><div class='innerbox'><div class='col'><div class='roll'><a href='http://en.wikipedia.org/wiki/" + show_list[cell + 1] + "'>" + show_list[cell + 1] + "</a></div></div></div></div>";
+  }
+}
+function getAllAnimeYears(){
+  for(var i = 1961; i <= 2015; i++){
+    loadWikiData(i);
+  }
+}
+function loadWikiData(year)
 {
     var xmlhttp;
     if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
+    {
         xmlhttp=new XMLHttpRequest();
     }
     xmlhttp.onreadystatechange=function()
@@ -24,23 +43,17 @@ function loadXMLDoc()
       if (xmlhttp.readyState==4 && xmlhttp.status==200)
       {
             var wiki_list = xmlhttp.responseText;
-            var wiki_html = $(".mw-category", wiki_list);
-            count = 0; 
+            var wiki_html = $("#mw-pages", wiki_list);
             $("a", wiki_html).each(function(){
-              show_list[count] = $(this).attr("title");
-              count++;
+              if($(this).attr("title") != "undefined" && $(this).attr("title").length != 0){
+                show_list[numAnime] = $(this).attr("title");
+                numAnime++;
+              }
             });
+            createTable();
       }
+      
     }
-    xmlhttp.open("GET","http://en.wikipedia.org/wiki/Category:2015_anime_television_series",true);
+    xmlhttp.open("GET","http://en.wikipedia.org/wiki/Category:" + year +"_anime_television_series",true);
     xmlhttp.send();
 }
-/*
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('popup.html', {
-    'bounds': {
-      'width': 800,
-      'height': 800
-    }
-  });
-});*/
