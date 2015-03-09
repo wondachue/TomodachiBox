4c1d9b4b669f478a9c5136b2d4e1186a2cd65e62
wondachue <megddwyer@gmail.com>
2015-03-09 11:58:57 -0400

    Progress in getting pics. Some small changes

--- popup.js
+++ popup.js
@@ -1,10 +1,9 @@
 var numAnime = 0;
 
-var show_list = [
-      "Hamtaro",
-      "Pokemon",
-      "Dragon Ball"
-    ];
+var show_list = [];
+var images = [];
+var descrips = [];
+
 $(function() {
     $( "#shows" ).autocomplete({
       source: show_list
@@ -16,20 +15,47 @@ $(document).ready(function() {
 });
 function createTable(){
   var table = document.getElementById("show_grid");
-  for(var cell = 0; cell < show_list.length; cell+=2){
+  for(var cell = 0; cell < 10; cell+=2){
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-
     row.className = "row";
-    cell1.innerHTML = "<div class='box'><div class='innerbox'><div class='col'><div class='roll'><a href='http://en.wikipedia.org/wiki/" + show_list[cell] + "'>" + show_list[cell] + "</a></div></div></div></div>";
-    cell2.innerHTML = "<div class='box'><div class='innerbox'><div class='col'><div class='roll'><a href='http://en.wikipedia.org/wiki/" + show_list[cell + 1] + "'>" + show_list[cell + 1] + "</a></div></div></div></div>";
+    var temp = "http://media1.popsugar-assets.com/files/2014/02/18/159/n/28443503/5945b74bba9e1117_shutterstock_105361820.jpg.xxxlarge/i/Calories-Sushi.jpg";
+    cell1.innerHTML = "<div class='box'><div class='innerbox'><div class='col'><div class='roll'><a href='http://en.wikipedia.org/wiki/" + show_list[cell] + "'>" + show_list[cell] + "</a><br><img id = 'showid_" + cell + "' src ='" + temp + "' class='img-circle sushi'></img></div></div></div></div>";
+    cell2.innerHTML = "<div class='box'><div class='innerbox'><div class='col'><div class='roll'><a href='http://en.wikipedia.org/wiki/" + show_list[cell + 1] + "'>" + show_list[cell + 1] + "</a><br><img id = 'showid_" + cell + "' src ='" + temp + "' class='img-circle sushi'></img></div></div></div></div>";
   }
 }
 function getAllAnimeYears(){
   for(var i = 1961; i <= 2015; i++){
     loadWikiData(i);
   }
+}
+function loadShowData(this_num)
+{
+    var xmlhttp;
+    if (window.XMLHttpRequest)
+    {
+        xmlhttp=new XMLHttpRequest();
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+      if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+            var wiki_page = xmlhttp.responseText;
+            var wiki_infobox = $(".infobox", wiki_page);
+            images[this_num] = "http:" + $("img", wiki_infobox).attr('src');
+            var wiki_descrip = $("#mw-content-text", wiki_page);
+            descrips[this_num] = $("p", wiki_descrip).first();
+            console.log("trying: http://en.wikipedia.org/wiki/" + show_list[this_num]);
+            console.log("showid_" + this_num);
+            console.log("stored src: " + $("img", wiki_infobox).attr('src'));
+            document.getElementById("showid_" + this_num).src = images[this_num];
+            console.log("new src: " + document.getElementById("showid_" + this_num).src);
+      }
+      
+    }
+    xmlhttp.open("GET","http://en.wikipedia.org/wiki/" + show_list[this_num],true);
+    xmlhttp.send();
 }
 function loadWikiData(year)
 {
@@ -45,12 +71,14 @@ function loadWikiData(year)
             var wiki_list = xmlhttp.responseText;
             var wiki_html = $("#mw-pages", wiki_list);
             $("a", wiki_html).each(function(){
-              if($(this).attr("title") != "undefined" && $(this).attr("title").length != 0){
+              if($(this).attr("title") != "undefined" && $(this).attr("title").length != 0 && $(this).attr("title") != "Wikipedia:FAQ/Categories"){
                 show_list[numAnime] = $(this).attr("title");
+                loadShowData(numAnime);
                 numAnime++;
               }
             });
             createTable();
+                
       }
       
     }
